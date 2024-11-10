189a03029bb25618b34934ac7f01fc721231f66a
Time:D <darya.sam54@gmail.com>
2024-11-11 03:57:13 +0700

    Add screen objects and transition(buttons)

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 from kivy.lang import Builder
 from kivy.core.window import Window
 
@@ -8,17 +8,24 @@ Builder.load_file('main.kv')
 class FirstScreen(Screen):
     pass
 
-class SecondScreen(Screen):
+class ObjectFirstScreen(Screen):
+    pass
+
+class ObjectSecondScreen(Screen):
+    pass
+
+class ObjectThirdScreen(Screen):
     pass
 
 class MyApp(App):
     def build(self):
         Window.size = (700, 900)
         sm = ScreenManager()
-        sm.add_widget(FirstScreen(name='first'))
-        sm.add_widget(SecondScreen(name='second'))
+        sm.add_widget(FirstScreen(name='menu'))
+        sm.add_widget(ObjectFirstScreen(name='first_object'))
+        sm.add_widget(ObjectSecondScreen(name='second_object'))
+        sm.add_widget(ObjectThirdScreen(name='third_object'))
         return sm
 
 if __name__ == '__main__':
     MyApp().run()
-
